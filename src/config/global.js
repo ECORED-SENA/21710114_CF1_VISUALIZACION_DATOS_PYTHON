e37b5e1ac996655523ae5e933aeebae9a597a402
flizcanor <flizcanor@icloud.com>
2023-10-12 22:47:06 -0500

    se agrega pdf accesible

--- src/config/global.js
+++ src/config/global.js
@@ -170,7 +170,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/21710114-CF01-DU.zip',
       },
       {
         icono: 'fas fa-download',
